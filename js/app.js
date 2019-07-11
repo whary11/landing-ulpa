964442325b0ee2343762481775862cf97264d64c
Luis Fernando Raga <whary11@gmail.com>
2019-07-11 12:52:32 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+new Vue({
+    el: '#app',
+    data: {
+        registro: {
+            nombre: '',
+            email: ''
+        },
+        error:false,
+    },
+    created() {
+
+
+    },
+    methods: {
+        scroll() {
+            $('html, body').animate({
+                scrollTop: $("div #contacto").offset().top
+            }, 1500)
+        },
+        send() {
+            if (this.registro.nombre.trim() == '') {
+                this.error = 'El nombre es incorrecto.'
+            }else if(this.registro.email.trim() == ''){
+                this.error = 'El email es incorrecto.'
+            }else{
+                this.error = 'El email es incorrecto.'
+            }
+        }
+    },
+})
